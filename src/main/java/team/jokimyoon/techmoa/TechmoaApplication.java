e90cc44fd4948280294cb4f39808821ea1dad087
sju3358 <tjdfkr011.dev@gmail.com>
2025-01-13 09:46:54 +0900

    inti : init techmoa project

---
+++ src/main/java/team/jokimyoon/techmoa/TechmoaApplication.java
@@ -0,0 +1,13 @@
+package team.jokimyoon.techmoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechmoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechmoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/jokimyoon/techmoa/TechmoaApplicationTests.java
@@ -0,0 +1,13 @@
+package team.jokimyoon.techmoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechmoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

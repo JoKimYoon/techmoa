b0ef186f7371ccf48e904fdd9520476612e00b46
sju3358 <tjdfkr011.dev@gmail.com>
2025-02-04 21:43:11 +0900

    feat : Oauth User Load 기능 추가 Oauth User Custom Service 개발 완료 User Entity 컬럼 추가

---
+++ src/main/java/team/jokimyoon/techmoa/domain/user/User.java
@@ -0,0 +1,71 @@
+package team.jokimyoon.techmoa.domain.user;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team.jokimyoon.techmoa.global.security.oauth.Oauth2Provider;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column
+	private String uuid;
+
+	@Column
+	@Enumerated(value = EnumType.STRING)
+	private Oauth2Provider oauthProvider;
+
+	@Column
+	private String oauthId;
+
+	@Column
+	private String nickname;
+
+	@Column
+	private String email;
+
+	@Column
+	private String profileImgUrl;
+
+	@Builder
+	public User(String oauthId, Oauth2Provider oauthProvider, String nickname, String email, String profileImgUrl) {
+		this.uuid = UUID.randomUUID().toString();
+		this.oauthId = oauthId;
+		this.oauthProvider = oauthProvider;
+		this.nickname = nickname;
+		this.email = email;
+		this.profileImgUrl = profileImgUrl;
+	}
+
+	public void changeProfileImgUrl(String profileImgUrl) {
+		this.profileImgUrl = profileImgUrl;
+	}
+
+	public void changeNickname(String nickname) {
+		//Todo 길이 제한
+		this.nickname = nickname;
+	}
+
+	public void changeEmail(String email) {
+		//Todo 길이 제한 , email validation
+		this.email = email;
+	}
+}
--- src/main/java/team/jokimyoon/techmoa/global/security/oauth/service/Oauth2UserCustomService.java
+++ src/main/java/team/jokimyoon/techmoa/global/security/oauth/service/Oauth2UserCustomService.java
@@ -7,10 +7,13 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import team.jokimyoon.techmoa.domain.user.repository.User;
+import team.jokimyoon.techmoa.domain.user.User;
+import team.jokimyoon.techmoa.domain.user.UserRepository;
 import team.jokimyoon.techmoa.global.exception.BusinessException;
 import team.jokimyoon.techmoa.global.security.oauth.Oauth2CustomUser;
 import team.jokimyoon.techmoa.global.security.oauth.Oauth2Provider;
@@ -18,27 +21,30 @@ import team.jokimyoon.techmoa.global.security.oauth.Oauth2Provider;
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(propagation = Propagation.REQUIRES_NEW)
 public class Oauth2UserCustomService extends DefaultOAuth2UserService {
+
+	private final UserRepository userRepository;
 
 	@Override
 	public OAuth2User loadUser(OAuth2UserRequest oAuth2UserRequest) throws OAuth2AuthenticationException {
 		OAuth2User oAuth2user = super.loadUser(oAuth2UserRequest);
 
-		oAuth2user.getAttributes()
-
 		try {
 
 			Oauth2Provider oauth2Provider = determineProvider(oAuth2UserRequest);
-			OAuth2User Oauth2User = null;
+
+			Oauth2CustomUser oAuth2User;
+
 			switch (oauth2Provider) {
-				case GOOGLE -> {
-					OAuth2User loadGoogleUser(oAuth2user, oauth2Provider);
-				}
-				case GITHUB -> {
-					OAuth2User loadGithubUser(oAuth2user, oauth2Provider);
-				}
+				case GOOGLE -> oAuth2User = loadGoogleUser(oAuth2user, oauth2Provider);
+				case GITHUB -> oAuth2User = loadGithubUser(oAuth2user, oauth2Provider);
 				default -> throw new OAuth2AuthenticationException("Unsupported Oauth2 provider");
 			}
+
+			updateUser(oAuth2User);
+
+			return oAuth2User;
 
 		} catch (Exception ex) {
 			log.info("[Oauth2UserCustomService] function : loadUser | error : {}", ex.getMessage());
@@ -56,7 +62,8 @@ public class Oauth2UserCustomService extends DefaultOAuth2UserService {
 		}
 	}
 
-	private Oauth2CustomUser loadGithubUser(OAuth2User oAuth2User, Oauth2Provider oauth2Provider) throws OAuth2AuthenticationException {
+	private Oauth2CustomUser loadGithubUser(OAuth2User oAuth2User, Oauth2Provider oauth2Provider) throws
+		OAuth2AuthenticationException {
 		Map<String, Object> userAttribute = oAuth2User.getAttributes();
 		String oauth2Id = userAttribute.get("id").toString();
 		String profileImageUrl = userAttribute.get("avatar_url").toString();
@@ -73,7 +80,8 @@ public class Oauth2UserCustomService extends DefaultOAuth2UserService {
 
 	}
 
-	private Oauth2CustomUser loadGoogleUser(OAuth2User oAuth2User, Oauth2Provider oauth2Provider) throws OAuth2AuthenticationException {
+	private Oauth2CustomUser loadGoogleUser(OAuth2User oAuth2User, Oauth2Provider oauth2Provider) throws
+		OAuth2AuthenticationException {
 		Map<String, Object> userAttribute = oAuth2User.getAttributes();
 		String profileImageUrl = userAttribute.get("picture").toString();
 		String nickname = userAttribute.get("name").toString();
@@ -89,15 +97,21 @@ public class Oauth2UserCustomService extends DefaultOAuth2UserService {
 			.build();
 	}
 
-	private User updateUser(Oauth2CustomUser oauth2CustomUser) {
+	private void updateUser(Oauth2CustomUser oauth2CustomUser) {
 
+		User user = userRepository.findUserBy(oauth2CustomUser.getOauth2Id(), oauth2CustomUser.getOauth2Provider())
+			.orElse(User.builder()
+				.oauthId(oauth2CustomUser.getOauth2Id())
+				.oauthProvider(oauth2CustomUser.getOauth2Provider())
+				.email(oauth2CustomUser.getEmail())
+				.nickname(oauth2CustomUser.getNickname())
+				.profileImgUrl(oauth2CustomUser.getProfileImgUrl())
+				.build());
 
+		user.changeEmail(oauth2CustomUser.getEmail());
+		user.changeNickname(oauth2CustomUser.getNickname());
+		user.changeProfileImgUrl(oauth2CustomUser.getProfileImgUrl());
 
-
-		userRepository.save(newUser);
-
-		log.info("[OAuth2UserServiceCustom] function : registerUser | message : 회원 가입 완료");
-
-		return newUser;
+		userRepository.saveAndFlush(user);
 	}
 }

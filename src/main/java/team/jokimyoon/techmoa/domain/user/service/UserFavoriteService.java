4a7e7a373d6e7eb507e7d90ead5a8ee2547441d4
sju3358 <tjdfkr011.dev@gmail.com>
2025-02-20 10:32:38 +0900

    chore : User Favorite Service 클래스만 추가

--- src/main/java/team/jokimyoon/techmoa/domain/user/service/UserFavoriteService.java
+++ src/main/java/team/jokimyoon/techmoa/domain/user/service/UserFavoriteService.java
@@ -1,11 +1,8 @@
 package team.jokimyoon.techmoa.domain.user.service;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
-import team.jokimyoon.techmoa.domain.user.model.dto.UserFavoriteDto;
 import team.jokimyoon.techmoa.domain.user.repository.UserFavoriteRepository;
 
 @Service
@@ -14,5 +11,4 @@ public class UserFavoriteService {
 
 	private final UserFavoriteRepository userFavoriteRepository;
 
-	public List<UserFavoriteDto>
 }

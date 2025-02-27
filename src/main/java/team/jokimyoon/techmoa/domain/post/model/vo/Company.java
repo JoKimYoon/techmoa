6ddc21af5a4666a2a1e37527c73dd174cd4fb6f8
sju3358 <tjdfkr011.dev@gmail.com>
2025-02-27 14:46:28 +0900

    feat : Enum From String method 추가

--- src/main/java/team/jokimyoon/techmoa/domain/post/model/vo/Company.java
+++ src/main/java/team/jokimyoon/techmoa/domain/post/model/vo/Company.java
@@ -1,7 +1,10 @@
 package team.jokimyoon.techmoa.domain.post.model.vo;
 
+import java.util.Locale;
+
 import lombok.Getter;
 import team.jokimyoon.techmoa.domain.collector.model.vo.CollectorName;
+import team.jokimyoon.techmoa.global.exception.BusinessException;
 
 @Getter
 public enum Company {
@@ -10,6 +13,15 @@ public enum Company {
 	TOSS(CollectorName.TOSS);
 
 	private final String collectorName;
+
+	public static Company of(String name) {
+		return switch (name.toUpperCase(Locale.ROOT)) {
+			case "KURLY" -> KURLY;
+			case "LINE" -> LINE;
+			case "TOSS" -> TOSS;
+			default -> throw new BusinessException("Invalid company name: " + name);
+		};
+	}
 
 	Company(String collectorName) {
 		this.collectorName = collectorName;

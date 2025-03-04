f1932f1383313600898efbbf1c38161f6db34ed4
sju3358 <tjdfkr011.dev@gmail.com>
2025-03-04 20:43:26 +0900

    feature : A.title DES 추가

--- src/main/java/team/jokimyoon/techmoa/domain/post/repository/dao/PostRepositoryCustomJpa.java
+++ src/main/java/team/jokimyoon/techmoa/domain/post/repository/dao/PostRepositoryCustomJpa.java
@@ -25,7 +25,7 @@ public interface PostRepositoryCustomJpa extends JpaRepository<Post, Long> {
 		FROM post A
 		LEFT JOIN post_company B FORCE INDEX (`PRIMARY`) ON A.post_company_id = B.id
 		WHERE A.published_at < :lastPublishedAt
-		ORDER BY A.published_at DESC
+		ORDER BY A.published_at DESC, A.title DESC
 		LIMIT :limitSize;
 		""", nativeQuery = true)
 	List<PostProjection> findAllByPublishedAtDesc(LocalDateTime lastPublishedAt, int limitSize);
